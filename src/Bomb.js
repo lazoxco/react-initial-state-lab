2e9c9088d6ddb7f7d2e24f7acf4ebd2a66b778b4
github username <github email address>
2021-05-26 23:23:50 -0400

    Added Bomb component.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,5 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends React.Component {
+
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,6 +1,5 @@
-// your ImageSlider code here!
 import React, { Component } from 'react';
 
 class ImageSlider extends React.Component {
-  
+
 }
