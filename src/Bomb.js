05c069acfec4ece55b9d04b6d83329f40890b98f
github username <github email address>
2021-05-26 23:55:13 -0400

    Added currentSlideIndex to initial state.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 export default class Bomb extends Component {
+
   render() {
     return <h1>Hello from Bomb</h1>
   }
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 export default class ImageSlider extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
   render() {
-    return <h1>Hello from ImageSlider</h1>
+    return <div>I am on slide {this.state.currentSlideIndex}</div>
   }
 }
